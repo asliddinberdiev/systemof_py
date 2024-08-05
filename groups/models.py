85fb955459d32963771ed522ac682327693f5446
asliddinberdiev <asliddinwork@gmail.com>
2024-08-05 14:23:10 +0500

    edit group

--- groups/models.py
+++ groups/models.py
@@ -3,7 +3,7 @@ import os
 
 class Group(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    day = models.BooleanField(),
+    day = models.BooleanField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     created_at = models.DateTimeField(auto_now=True)
