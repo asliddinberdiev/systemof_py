c25c379bc9ae81e1ec74266658a3ae21ac7b685a
asliddinberdiev <asliddinwork@gmail.com>
2024-08-10 13:30:14 +0500

    add excel

--- pupils/urls.py
+++ pupils/urls.py
@@ -1,8 +1,11 @@
+from django.urls import path
+from .views import PupilViewSet  # Ensure PupilViewSet is imported from the correct module
 from rest_framework.routers import SimpleRouter
-from .views import PupilViewSet
 
 router = SimpleRouter()
 router.register('pupils', PupilViewSet, basename='pupils')
 
-
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    # Manually add the export_excel route
+    path('pupils/export_excel/', PupilViewSet.as_view({'get': 'export_excel'}), name='pupil-export-excel'),
+]
--- pupils/views.py
+++ pupils/views.py
@@ -1,14 +1,31 @@
+import pandas as pd
+from django.http import HttpResponse
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
-from .serializers import PupilSerializer
 from .models import Pupil
+from .serializers import PupilSerializer
 
-# Create your views here.
 class PupilViewSet(ModelViewSet):
     queryset = Pupil.objects.all()
     serializer_class = PupilSerializer
     filter_backends = (SearchFilter,)
-    search_fields = ["firstname", "lastname", "pupil_phone", "parent_phone", "group__name", ]
+    search_fields = ["firstname", "lastname", "pupil_phone", "parent_phone", "group__name"]
 
+    def export_excel(self, request, *args, **kwargs):
+        # Retrieve filtered Pupil records
+        pupils = self.filter_queryset(self.get_queryset())
 
+        # Serialize the queryset
+        pupil_data = PupilSerializer(pupils, many=True).data
 
+        # Convert to DataFrame
+        df = pd.DataFrame(pupil_data)
+
+        # Create an HttpResponse object with Excel MIME type
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=pupils.xlsx'
+
+        # Write the DataFrame to the response
+        df.to_excel(response, index=False, engine='openpyxl')
+
+        return response
